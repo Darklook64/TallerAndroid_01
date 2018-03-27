8c0039461e58212ac65b875e9096625acd6af806
ChaosAlchemist <jrcortest1@gmail.com>
2018-03-26 22:26:55 -0300

    Se agregan los métodos para cargar imagen y generar número random. Se agrega de manera temporal el numero generado a un label.

--- app/src/main/java/com/example/dell/taller/Principal.java
+++ app/src/main/java/com/example/dell/taller/Principal.java
@@ -2,12 +2,47 @@ package com.example.dell.taller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class Principal extends AppCompatActivity {
+    private EditText txtNumero;
+    private TextView lblMensaje;
+    private ImageView imgVictoria;
+    private int numeroR;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        generarNumero();
+    }
+
+    public void generarNumero(){
+        // Genera un número random con un límite de 1000.
+        Random r = new Random();
+        numeroR = r.nextInt(1001);
+
+        lblMensaje = (TextView) findViewById(R.id.lblMensaje);
+        lblMensaje.setText("El número generado es: "+numeroR);
+
+
+    }
+
+    // Cuando gana, se pone la imagen y un label de felicidades.
+    // Cargar este método cuando el usuario
+    public void recompensarUsuario(){
+        imgVictoria = (ImageView) findViewById(R.id.imgVictoria);
+        lblMensaje = (TextView) findViewById(R.id.lblMensaje);
+
+        lblMensaje.setText("¡Felicidades, ha ganado!");
+        imgVictoria.setImageResource(R.drawable.oraora);
+
+
     }
 }
